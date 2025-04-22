1b0b64d31287b372bf8e34d850d01f5465db22b3
James McEwan <60427+jamesmcewan@users.noreply.github.com>
2025-04-22 10:11:51 +0100

    oxlint

--- .prettier.mjs
+++ .prettier.mjs
@@ -1,20 +1,6 @@
+/** @type {import("prettier").Config} */
 export default {
-  semi: false,
-  singleQuote: true,
-  tabWidth: 2,
-  trailingComma: "all",
-  printWidth: 100,
-
-  // Add explicit values for these options
-  bracketSpacing: true,
-  bracketSameLine: false,
-  arrowParens: "avoid",
-  endOfLine: "lf",
-
-  // Provide an ordered list of plugins
   plugins: ["prettier-plugin-astro", "prettier-plugin-tailwindcss"],
-
-  // Expanded overrides
   overrides: [
     {
       files: "*.astro",
@@ -22,26 +8,8 @@ export default {
         parser: "astro",
       },
     },
-    {
-      files: "*.ts",
-      options: {
-        parser: "typescript",
-      },
-    },
-    {
-      // Handle JSON files
-      files: ["*.json", "*.jsonc"],
-      options: {
-        parser: "json",
-        trailingComma: "none", // Avoid syntax errors in JSON files
-      },
-    },
-    {
-      // Handle markdown files
-      files: ["*.md", "*.mdx"],
-      options: {
-        proseWrap: "always",
-      },
-    },
   ],
+  trailingComma: "all",
+  singleQuote: true,
+  semi: false,
 };
