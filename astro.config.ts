a7c562a694d27eebd3981ac0844571939c43a30f
James McEwan <60427+jamesmcewan@users.noreply.github.com>
2025-04-22 15:42:21 +0100

    biome intead of prettier

--- astro.config.ts
+++ astro.config.ts
@@ -1,10 +1,10 @@
-import tailwindcss from '@tailwindcss/vite'
-import { defineConfig } from 'astro/config'
+import tailwindcss from "@tailwindcss/vite"
+import { defineConfig } from "astro/config"
 
 export default defineConfig({
-  output: 'static',
-  site: 'https://mightydinosaur.dev/',
-  vite: {
-    plugins: [tailwindcss()],
-  },
+	output: "static",
+	site: "https://mightydinosaur.dev/",
+	vite: {
+		plugins: [tailwindcss()],
+	},
 })
