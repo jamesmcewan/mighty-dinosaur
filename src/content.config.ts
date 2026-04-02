e25f9f67670da2d52fdd0dca8c48073c2e610d4d
James McEwan <james@dinobytes.dev>
2026-04-02 13:48:19 +0100

    feat: things

--- src/content.config.ts
+++ src/content.config.ts
@@ -4,5 +4,14 @@ import comics from "./config/comics"
 import movies from "./config/movies"
 import music from "./config/music"
 import elsewhere from "./config/elsewhere"
+import things from "./config/things"
 
-export const collections = { posts, pages, movies, music, comics, elsewhere }
+export const collections = {
+  posts,
+  pages,
+  movies,
+  music,
+  comics,
+  elsewhere,
+  things,
+}
